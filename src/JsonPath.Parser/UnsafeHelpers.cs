f1b48fcae09a505773ad3e50ccb0182ab651814b
Michael Yarichuk <michael.yarichuk@gmail.com>
2025-11-13 10:31:05 +0200

    fix: avoid release allocator tracking allocations

--- src/JsonPath.Parser/UnsafeHelpers.cs
+++ src/JsonPath.Parser/UnsafeHelpers.cs
@@ -10,6 +10,24 @@ public class UnsafeHelpers
         var size = Unsafe.SizeOf<T>();
 
         // assume at least pointer-size alignment (worst case bit over-align)
-        return size < IntPtr.Size ? IntPtr.Size : size;
+        var required = size < IntPtr.Size ? IntPtr.Size : size;
+        return RoundUpToPowerOfTwo(required);
+    }
+
+    private static int RoundUpToPowerOfTwo(int value)
+    {
+        if (value <= 0)
+        {
+            return 1;
+        }
+
+        value--;
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+        value++;
+        return value;
     }
 }
